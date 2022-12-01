12de44bbe1e113ad961e2aac13e9989a28fc2bc1
Rahul Ashokkumar Macwan <rahulmac17599@gmail.com>
2022-12-01 11:56:05 -0400

    Update UserPool.js

--- src/Configs/UserPool.js
+++ src/Configs/UserPool.js
@@ -2,8 +2,8 @@ import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
 
-  UserPoolId: 'us-east-2_MVK3l0blZ',
-  ClientId: '31u8ucf1g2na8ue7buerln0v1j',
+  UserPoolId: 'USERPOOL ID',
+  ClientId: 'CLIENT ID',
 };
 
 export default new CognitoUserPool(poolData);
